e65378cad778db405fac552f637f836d8100fafb
shicks <shicks255@yahoo.com>
2018-04-18 21:41:08 -0400

    more sports store fun

--- sportsStore/app/app.component.ts
+++ sportsStore/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "app",
-    template: `<div class="bg-success">
-                    This is SportsStore
-               </div>`
+    template: "<store></store>"
 })
 export class AppComponent {}
---
+++ sportsStore/app/model/model.module.ts
@@ -0,0 +1,10 @@
+import {Injectable, NgModule} from "@angular/core";
+import {ProductRepository} from "./product.repository";
+import {StaticDataSource} from "./static.datasource";
+
+@NgModule({
+    providers: [ProductRepository, StaticDataSource]
+})
+export class ModelModule {
+    
+}
