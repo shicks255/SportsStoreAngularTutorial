6d265a14c1583e4834a056b9d7cb32caf88d1fc9
shicks <shicks255@yahoo.com>
2018-04-19 21:54:46 -0400

    adding pagination, but some kinf of weird bug still going on when changing items per page

--- sportsStore/app/store/store.component.ts
+++ sportsStore/app/store/store.component.ts
@@ -8,16 +8,49 @@ import {ProductRepository} from "../model/product.repository";
     templateUrl: "store.component.html"
 })
 export class StoreComponent{
+    public selectedCategory = null;
+    public productsPerPage = 4;
+    public selectedPage = 1;
+
     constructor(private repository: ProductRepository)
     {}
 
     get products(): Product[]
     {
-        return this.repository.getProducts();
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        console.log(
+            this.repository
+                .getProducts(this.selectedCategory)
+                .splice(pageIndex, pageIndex + this.productsPerPage));
+        return this.repository.getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories(): string[]
     {
         return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory?: string)
+    {
+        this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage: number)
+    {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: number)
+    {
+        this.productsPerPage = newSize;
+        this.changePage(1);
+    }
+
+    get pageNumbers(): number[]
+    {
+        return Array(Math.ceil(this.repository
+            .getProducts(this.selectedCategory).length / this.productsPerPage))
+            .fill(0).map((x,i) => i + 1);
     }
 }
