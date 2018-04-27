9999283a55b683e3913e9bdaa0da440a6296d0b3
shicks <shicks255@yahoo.com>
2018-04-26 22:15:54 -0400

    adding to the cart detail page

--- sportsStore/app/store/cartDetail.component.ts
+++ sportsStore/app/store/cartDetail.component.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { Cart} from "../model/cart.model";
 
 @Component({
-    template: `<div><h3 class="bg-info p-1">Cart Detail Component</h3></div>`
+    moduleId: module.id,
+    templateUrl: "cartDetail.component.html"
 })
 export class CartDetailComponent {
-    
+    constructor (public cart: Cart)
+    {}
 }
